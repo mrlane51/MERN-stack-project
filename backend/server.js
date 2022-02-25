6a9479d54a40a08bb782e08574d11853b75ab227
Lane Lopez <mrlane51@ymail.com>
2022-02-25 10:03:43 -0600

    added first route & logic

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,40 @@
 const mongoose = require('mongoose')
-const User = require('./user')
+const User = require('./User')
 
 mongoose.connect('mongodb://localhost/merndb', () => {
     console.log('connected')
 },
-e => console.error(e))
+    e => console.error(e))
 
 async function run() {
-    const user = new User({ name: 'tim', age: '42'})
-    await user.save()
-    console.log('user saved')
+    try {
+    const user = await User.create({
+        name: 'jill doe',
+        jobTitle: 'sales manager',
+        email: 'jill41@mern.com',
+        department: 'sales',
+        groups: ['managers', 'sales team'],
+        workTime: {
+            morning: true,
+            afternoon: true,
+            overnight: false
+        }
+    })
+    console.log(user)
+    // catch errors if input type doesnt match
+    } catch (e) {
+        console.log(e.message)
+    }
 }
+run()
 
-// run()
+// Router.get('/findall', (req, res) => {
+//     User.find({}, (err, result) => {
+//         if (result) {
+//             res.status(200).json(result);
+//         } else {
+//             console.log('Uh Oh, something went wrong');
+//             res.status(500).json({ message: 'something went wrong' });
+//         }
+//     })
+// })
--- backend/user.js
+++ backend/user.js
@@ -1,16 +1,33 @@
 // pulling in the mogoose module
 const mongoose = require('mongoose')
 
+// for other nested objects : option 2 instead of inside the first schema
+const addressSchema= new mongoose.Schema({
+    street: String,
+    city: String,
+})
+
 // userSchema collection object with fields for the document
 const userSchema = new mongoose.Schema({
     // fields aka key: value pairs
     name: String,
-    age: Number,
-    jobTitle: String
+    jobTitle: String,
+    email: String,
+    // reference to another user object
+    department: mongoose.SchemaTypes.ObjectId,
+    // array of strings
+    groups: [String],
+    // nested array of objects
+    workTime: {
+        morning: Boolean,
+        afternoon: Boolean,
+        overnight: Boolean,
+    },
+    address: addressSchema
 })
 
 // model for user documents in our userSchema collection
-const users = mongoose.model('users', userSchema)
+module.exports = mongoose.model('User', userSchema)
 
-
-module.exports = users;
+// const users = mongoose.model('Users', userSchema)
+// module.exports = users;
